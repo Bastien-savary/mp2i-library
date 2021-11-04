18d7b503da8edda94e712ba37ec477547add5827
Bastien-savary <89684937+Bastien-savary@users.noreply.github.com>
2021-11-04 19:26:52 +0100

    Update zipper.ml

--- bibliothèque/listes complexes/zipper/zipper.ml
+++ bibliothèque/listes complexes/zipper/zipper.ml
@@ -1,1 +1,11 @@
+(*type d'un zipper*)
+type 'a zipper = { left : 'a list; right : 'a list};;
 
+(*déplacer le curseur d'un zipper vers la droite ou la gauche*)
+let move_right z = match z.right with 
+  |[] -> failwith "empty right"
+  |e::q -> {left = e::z.left; right = q};;
+  
+ let move_left z = match z.left with 
+  |[] -> failwith "empty left"
+  |e::q -> {left = q; right = e::z.right};;
