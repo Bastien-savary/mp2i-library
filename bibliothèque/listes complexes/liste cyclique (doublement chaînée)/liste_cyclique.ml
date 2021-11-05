e1b0944996a333e9e3e2401f4ab45508d70c220d
Bastien-savary <89684937+Bastien-savary@users.noreply.github.com>
2021-11-05 10:08:36 +0100

    Update liste_cyclique.ml

--- bibliothèque/listes complexes/liste cyclique (doublement chaînée)/liste_cyclique.ml
+++ bibliothèque/listes complexes/liste cyclique (doublement chaînée)/liste_cyclique.ml
@@ -1,1 +1,39 @@
+(*type de la liste cyclique*)
+type 'a l2c = {elem : 'a; mutable prev : 'a l2c; mutable next : 'a l2c};;
 
+(*pour créer une liste cyclique à 1 élément*)
+let create e = 
+      let rec l = {elem = e; prev = l; next = l} in l;;
+      
+(*[length l] renvoie la longueur de la liste l2c*)
+let rec aux l1 =
+  if l1 == l then 1 else 1 + aux l1.next in 
+  aux l.next;;
+  
+(*[appartient e l] indique si l'élément e appartien à l*)
+let appartient e l =
+  let cur = ref l.next in 
+  while !cur.elem <> e && !cur != l do 
+    cur := !cur.next
+  done;
+  !cur.elem = e;;
+
+(*[add l e] pour ajouter ou [del l e] supprimer un élément d'une l2c en compléxité O(1)*)
+let add l e = 
+    let l_new = {elem = e; prev = l; next = l.next} in 
+      l.next.prev <- l_new ; 
+      l.next <- l_new;
+      
+let del l = 
+    l.prev.next <- l.next;
+    l.next.prev <- l.prev
+    
+(*[fusion l1 l2]  réalise la fusion des deux listes en une*)
+let fusion l1 l2 = 
+  (l1.next).prev <- l2;
+  (l2.next).prev <- l1;
+  let l1n = l1.next in 
+  l1.next <- l2.next;
+  l2.next <- l1n;;
+  
+  
