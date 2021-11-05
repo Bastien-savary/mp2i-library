3318a54a6493e01c3ef8c40d38e24707c47ad8de
Bastien-savary <89684937+Bastien-savary@users.noreply.github.com>
2021-11-05 11:02:09 +0100

    Update and rename bibliothèque/table_hachage/table_hachage.ml to bibliothèque/table_hachage/table hachage classique/table_hachage.ml

--- bibliothèque/table_hachage/table hachage classique/table_hachage.ml
+++ bibliothèque/table_hachage/table hachage classique/table_hachage.ml
@@ -11,19 +11,34 @@ let appartient t k =
     |(k',_)::q -> k=k' || aux q in 
     aux t.donnees.(t.hache k);;
 
+let appartient_2 t k =
+    t.donnees.(t.hache k)
+    |> List.map fst 
+    |> List.mem k;;
+
 (* [element t k] ] retrourne l'élément associé à la clé k dans la table de hachage*)
 let element t k =
     let rec aux = function
-    | [] -> failwith "not in the table"
+    | [] -> failwith "la clé k n'existe pas"
     |(k',e)::q -> if k=k' then e
                 else aux q in
 aux t.donnees.(t.hache k);;
 
 (* [ajout t k e] ajoute l'élément e dans la table associé à la clé k *)
 let ajout t k e =
-    if recherche t k == true then failwith "key already here"
-    else t.donnees.(t.hache k) <- (k,e)::t.donnees.(t.hache k);;
+    if not (recherche t k) then 
+        t.donnees.(t.hache k) <- (k, e)::t.donnees.(t.hache k)
+        else failwith "la clé est déja présente"
 
 (* [suppr t k] supprime la clé k de la table de hachage*)
 let suppr t k =
     t.donnees.(t.hache k) <- List.filter (fun c -> fst c <> k) t.donnees.(t.hache k)
+    
+let suppr_2 t k =
+    if not (appartient t k) then failwith "pas de changement"
+    else let rec enlever = function 
+        | [] -> []
+        | (k', _):: q when k = k'  -> enlever q 
+        |e::q -> e::enlever q in 
+        t.donnees.(t.hache k) <- enlever (t.donnees.(t.hache k))    
+ 
