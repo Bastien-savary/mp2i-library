03065055905d59fb40f3f959752ad95427944f0e
Bastien-savary <89684937+Bastien-savary@users.noreply.github.com>
2021-11-27 19:40:19 +0100

    Update tableaux_indicateurs.ml

--- bibliothèque/tableaux/1 - indicateurs/tableaux_indicateurs.ml
+++ bibliothèque/tableaux/1 - indicateurs/tableaux_indicateurs.ml
@@ -88,7 +88,13 @@ let dichotomie_t e t1 =
 aux 0 ((Array.length t1)-1);;
 
 (* [trichotomie_t e t1] indique si e appartient à t1 en compléxité O(nlog(n))*)
-let trichotomie_t e t1 =
+let trichotomie e t1 =
     let rec aux i j =
     if i > j then false
     else let m1 = (2*i + j + 1)/3 in
+        let m2 =(i + 2*j +2)/3 in
+        if t1.(m1) = e || t1.(m2) = e then true
+        else if e< t1.(m1) then aux i (m1-1)
+        else if e< t1.(m1) then aux (m1+1) (m2-1)
+        else aux (m2+1) j in
+aux 0 ((Array.length t1)-1);;
